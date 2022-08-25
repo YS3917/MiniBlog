087d5b43facf4313313ac57cd29080c9e86c6609
yongseok <tjdydtjr85@gmail.com>
2022-08-25 13:14:51 +0900

    3차 인풋 검색창

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="wrap">
-  <BlogFooter />
   <BlogHeader />
   <BlogInput />
   <BlogList />
+  <BlogFooter />
 </div> 
 </template>
 
@@ -55,7 +55,16 @@ font-weight: 400;
 line-height: 1.2;
 letter-spacing: -0.64;
 color:#000;
-
+background-color: #f6f6f6;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.05);
+}
+.wrap{
+  position: relative;
+  display: block;
+  width: 90%;
+  margin: 0 auto;
 }
 
 </style>
